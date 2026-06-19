b0fb0e090620b5cb0dd0590ac267182ad9bc468d
Jacky <jacphu-7@student.ltu.sem>
2026-06-19 11:04:18 +0200

    update_104

--- python/get_better.py
+++ python/get_better.py
@@ -22,7 +22,6 @@
 # print(f"Balance after {time} year/s €{total:.2f}")
 
 
-
 # timer
 # import time
 
@@ -38,28 +37,105 @@
 # print("Time's up!")
 
 
-
 # Shopping cart program
-foods = []
-prices = []
-total = 0
+# foods = []
+# prices = []
+# total = 0
 
-while True:
-    food = input("Enter a food to buy (q to quit): ")
-    if food.lower() == "q":
-        break
-    else:
-        price = float(input(f"Enter the price of a {food}: $"))
-        foods.append(food)
-        prices.append(price)
+# while True:
+#     food = input("Enter a food to buy (q to quit): ")
+#     if food.lower() == "q":
+#         break
+#     else:
+#         price = float(input(f"Enter the price of a {food}: $"))
+#         foods.append(food)
+#         prices.append(price)
 
-print("----- Your cart -----")
+# print("----- Your cart -----")
 
-for food in foods:
-    print(food)
+# for food in foods:
+#     print(food)
 
-for price in prices:
-    total += price
+# for price in prices:
+#     total += price
 
-print()
-print(f"You total is: ${total}")
+# print()
+# print(f"You total is: ${total}")
+
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# vegetables = ["celery", "carrots", "potatoes"]
+# meats = ["chicken", "fish", "turkey"]
+
+# groceries = [fruits, vegetables, meats]
+
+# for collection in groceries:
+#     for food in collection:
+#         print(food, end=" ")
+#     print()
+
+
+# num_pad = (
+#     (1, 2, 3),
+#     (4, 5, 6,),
+#     (7, 8, 9),
+#     ("*", 0, "#"),
+# )
+
+# for row in num_pad:
+#     for num in row:
+#         print(num, end=" ")
+#     print()
+
+
+# Quiz game
+# questions = (("1"), 
+#              ("2"), 
+#              ("3"), 
+#              ("4"), 
+#              ("5"))
+
+# options = (("1", "2", "3", "4", "5"), 
+#            ("2", "2", "3", "4", "5"), 
+#            ("3", "2", "3", "4", "5"), 
+#            ("4", "2", "3", "4", "5"), 
+#            ("5", "2", "3", "4", "5"))
+
+# answers = ("1", "2", "3", "4", "5")
+# guesses = []
+# score = 0
+# question_num = 0
+
+# for question in questions:
+#     print("--------------------------")
+#     print(question)
+#     for option in options[question_num]:
+#         print(option)
+
+#     guess = input("Enter (1, 2, 3, 4, 5) ").upper()
+#     guesses.append(guess)
+#     if guess == answers[question_num]:
+#         score += 1
+#         print("Correct")
+#     else:
+#         print("incorrect")
+#         print(f"{answers[question_num]} is the correct answer")
+
+#     question_num += 1
+
+# print("--------------------------")
+# print("         Result           ")    
+# print("--------------------------")    
+
+# print("answers:", end=" ")
+# for answer in answers:
+#     print(answer, end=" ")
+# print()
+
+# print("guesses:", end=" ")
+# for guess in guesses:
+#     print(guess, end=" ")
+# print()
+
+# score = int(score / len(questions) * 100)
+# print(f"Your score is: {score}%")
