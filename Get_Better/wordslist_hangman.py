bc01db3a0e4332a3e80cc19cd9d81aa84087f11b
Jacky <jacphu-7@student.ltu.sem>
2026-06-22 20:26:42 +0200

    update_109

---
+++ Get_Better/wordslist_hangman.py
@@ -0,0 +1,6 @@
+# words for hangman game
+
+words = ("lion", "tiger", "elephant", "giraffe", "zebra",
+    "kangaroo", "panda", "penguin", "koala", "dolphin",
+    "wolf", "fox", "bear", "rabbit", "deer",
+    "monkey", "leopard", "cheetah", "hippopotamus", "camel")
